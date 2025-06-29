2ca8c77d0ea091f85cd4e6dd86db00deec68909d
Aditya Verma <adityaverma1205c@gmail.com>
2025-06-29 16:40:45 +0530

    Modified TTS BOT

--- Intermediate Python Projects/01_tts_bot.py
+++ Intermediate Python Projects/01_tts_bot.py
@@ -5,6 +5,9 @@ while True:
         engine = pyttsx3.init()
         text = input("Enter your text : ").lower()
         if(text == 'quit'):
+            engine.say("Byee Byee Aditya")
+            engine.say(text)
+            engine.runAndWait()
             break
         else:
             engine.say(text)
